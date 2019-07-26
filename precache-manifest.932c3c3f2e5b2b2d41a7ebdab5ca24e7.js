c32fc237cd283a459651e53ed0d22e1af383932b
Tejas H <tejas007tez@gmail.com>
2019-07-26 20:59:15 +0530

    Updates

---
+++ precache-manifest.932c3c3f2e5b2b2d41a7ebdab5ca24e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f97ef6c3fcc73d3e7f5f7f841b69104",
+    "url": "/BhagavadGeeta-App/index.html"
+  },
+  {
+    "revision": "a26be178d4734f75d2dd",
+    "url": "/BhagavadGeeta-App/static/css/main.c2b2bb00.chunk.css"
+  },
+  {
+    "revision": "bb31a0ea494632de5159",
+    "url": "/BhagavadGeeta-App/static/js/2.24c52790.chunk.js"
+  },
+  {
+    "revision": "a26be178d4734f75d2dd",
+    "url": "/BhagavadGeeta-App/static/js/main.364826b1.chunk.js"
+  },
+  {
+    "revision": "2d4361b57539b8b0980a",
+    "url": "/BhagavadGeeta-App/static/js/runtime~main.08eec82e.js"
+  }
+]);
